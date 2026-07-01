5ba66717c005118284a9e650d6bed674b28e9bae
saintenxt <mazart7707@gmail.com>
2026-07-01 12:43:44 +0300

    fix: some minor problems

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useNavigate } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../pages/Header';
 
 const navigate = useNavigate();
